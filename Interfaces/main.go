95134aa85ddca928918f16ba3a6dca01bb69c90d
Yuning Zhou <yuning.zhou@stern.nyu.edu>
2020-03-20 17:38:39 -0400

    Create main.go

---
+++ Interfaces/main.go
@@ -0,0 +1,39 @@
+package main
+
+import (
+	"fmt"
+	"math"
+)
+
+type Shape interface {
+	area() float64
+}
+
+type Circle struct {
+	x, y, radius float64
+}
+
+type Rectangle struct {
+	width, height float64
+}
+
+func (c Circle) area() float64 {
+	return math.Pi * c.radius * c.radius
+}
+
+func (r Rectangle) area() float64 {
+	return r.width * r.height
+}
+
+func getArea(s Shape) float64 {
+	return s.area()
+}
+
+func main() {
+	circle := Circle{1, 1, 1}
+	rect := Rectangle{2, 4}
+
+	fmt.Println("Circle area: ", getArea(circle))
+	fmt.Println("Rectangle area: ", getArea(rect))
+
+}
